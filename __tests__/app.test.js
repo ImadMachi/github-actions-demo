3b5f2c448afc2c06a70b05da4e8ed154364bc392
ImadMachi <machii.imad@gmail.com>
2023-12-14 16:05:41 +0100

    test that fail

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,7 +4,7 @@ const app = require("../app");
 describe("GET /", () => {
 	test('responds with "Hello World!"', async () => {
 		const response = await request(app).get("/");
-		expect(response.status).toBe(200);
+		expect(response.status).not.toBe(200);
 		expect(response.text).toBe("Hello World!");
 	});
 });
